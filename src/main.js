dd747ba572eca433eea0bd2d3232d4a7a1a50f79
LauraNsenga <laura.nsenga@eagles.oc.edu>
2025-04-01 11:47:34 -0500

    fix to merge

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from "./App.vue";
 import router from "./router/router";
 import vuetify from "./plugins/vuetify.js";
 import store from "./store/store.js";
+import "./assets/main.css"; // ✅ Import the global styles
 import '@fortawesome/fontawesome-free/css/all.css'
-
 
 // App.use(store); //added the store to help us access our store globally
 createApp(App)
